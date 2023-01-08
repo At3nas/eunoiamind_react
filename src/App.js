cb77815061df5be40ed78164536ca38c55f29a94
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2023-01-08 18:55:56 -0300

    Revert "Rutas en App.js"
    This reverts commit 2d4773fd6f02d386ff1858a21a9e2ad2129de35a.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import Main from './components/main';
-import Bienvenida from './components/bienvenida';
+import Welcome from './components/welcome';
+import Registro from './components/registro';
 import Muro from './components/muro';
-import Home from './components/home';
-import Calendario from './components/calendario';
-import Pregunta from './components/pregunta';
 import Configuraciones from './components/configuraciones';
 
 import './styles/css/App.css';
@@ -16,19 +14,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
-
-          <Route path="/bienvenida" element={<Bienvenida />} />
-
-          <Route path="/pregunta" element={<Pregunta />} />
-
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/registro" element={<Registro />} />
           <Route path="/muro" element={<Muro />} />
-
-          <Route path="/home" element={<Home />} />
-
-          <Route path="/calendario" element={<Calendario />} />
-
-          <Route path="/configuracion" element={<Configuraciones />} />
-          
+          <Route path="/configuraciones" element={<Configuraciones />} />
         </Routes>
       </Router>
     </div>
