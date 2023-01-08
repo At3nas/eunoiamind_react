02cbad2529c66c99ebcccb6a3c6fce8a3ac5e9ba
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2023-01-08 18:40:23 -0300

    Link en Bienvenida
    Agregué Link al botón de Bienvenida que lleva a la pantalla de Pregunta

--- src/components/bienvenida.jsx
+++ src/components/bienvenida.jsx
@@ -1,24 +1,25 @@
-import React,{Component} from 'react';
-import './styles/sass/styles.scss';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/sass/styles.scss';
 
-class Bienvenida extends Component{
-    render(){
-        return(
-            <div className="container-fluid" id="fondo">
-                <div className="contenedor">
-                    <h1 className="titulo-bienvenida">¡Bienvenid@ a EunoiaMind!</h1>
-                </div>
-                <div className="contenedor">
-                    <img className="logo" src="../src/assets/logo-manchita.png"alt/>
-                </div>
-                <div className="contenedor">
-                    <h2 className="subtitulo">Comencemos llenando algunos datos</h2>
-                </div>
-                <div className="contenedor">
-                    <a href="pregunta.html"><button type="button" className="btn1">Comenzar</button></a>
-                </div>
-            </div>
-        );
-    }
+class Bienvenida extends Component {
+  render() {
+    return (
+      <div className="container-fluid" id="fondo">
+        <div className="contenedor">
+          <h1 className="titulo-bienvenida">¡Bienvenid@ a EunoiaMind!</h1>
+        </div>
+        <div className="contenedor">
+          <img className="logo" src={require("../assets/logo-manchita.png")} alt="" />
+        </div>
+        <div className="contenedor">
+          <h2 className="subtitulo">Comencemos llenando algunos datos</h2>
+        </div>
+        <div className="contenedor">
+          <Link to="/pregunta"><button type="button" className="btn1">Comenzar</button></Link>
+        </div>
+      </div>
+    );
+  }
 }
 export default Bienvenida;
