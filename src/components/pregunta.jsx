0f1fe013bc623446dae8309760f2632251782ab1
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2023-01-08 18:37:54 -0300

    Link en Pregunta
    Agregué Link en el botón de Pregunta que lleva a la pantalla de Muro

--- src/components/pregunta.jsx
+++ src/components/pregunta.jsx
@@ -1,33 +1,34 @@
-import React,{Component} from 'react';
-import './styles/sass/styles.scss';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/sass/styles.scss';
 
-class Pregunta extends Component{
-   render(){
-    return(
+class Pregunta extends Component {
+  render() {
+    return (
       <div class="container-fluid div__pregunta">
-      <div>
-        <h1 class="titulo">¿como te sientes hoy?</h1>
-      </div>
-  
-      <div class="div__pregunta__emociones">
-          <img src={require("../src/assets/felicidad.png")} class="emocion" alt=''/>
-          <img src={require("../src/assets/miedo.png")} class="emocion" alt='' />
-          <img src={require("../src/assets/enojo.png")} class="emocion" alt='' />
-          <img src={require("../src/assets/disgusto.png")} class="emocion" alt='' />
-          <img src={require("../src/assets/sorpresa.png")} class="emocion" alt=''/>
-          <img src={require("../src/assets/tristeza.png")} class="emocion" alt='' />
+        <div>
+          <h1 class="titulo">¿Cómo te sientes hoy?</h1>
         </div>
-      
-      <div class="_mancha">
-        <img class="mancha" src={require("../src/assets/logo-manchita.png")} alt=''/>
+
+        <div class="div__pregunta__emociones">
+          <img src={require("../assets/felicidad.png")} class="emocion" alt='' />
+          <img src={require("../assets/miedo.png")} class="emocion" alt='' />
+          <img src={require("../assets/enojo.png")} class="emocion" alt='' />
+          <img src={require("../assets/disgusto.png")} class="emocion" alt='' />
+          <img src={require("../assets/sorpresa.png")} class="emocion" alt='' />
+          <img src={require("../assets/tristeza.png")} class="emocion" alt='' />
+        </div>
+
+        <div class="_mancha">
+          <img class="mancha" src={require("../assets/logo-manchita.png")} alt='' />
+        </div>
+
+        <div class="_btn">
+          <Link to="/muro"><button type="button" class="btn2">Enviar</button></Link>
+        </div>
       </div>
-  
-      <div class="_btn">
-        <button type="button" class="btn2">Enviar</button>
-      </div>
-    </div>
 
     );
-   }
+  }
 }
 export default Pregunta;
