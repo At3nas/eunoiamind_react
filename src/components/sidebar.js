7880d646d5b80cf4c5ddf996044f9adebe223a69
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2023-01-08 18:39:00 -0300

    Link en Sidebar
    Agregué Link en Sidebar que lleva a las pantallas correspondientes
    
    Por corregir: Home no se despliega

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/sass/styles.scss';
 
 class Sidebar extends Component {
-    render() {
-        return (
-            <nav className="nav nav--sidebar">
-                <img src="/src/assets/avatar_test.jpg" className="nav__avatar" alt="Avatar del usuario" />
+  render() {
+    return (
+      <nav className="nav nav--sidebar">
+        <img src={require("../assets/avatar_test.jpg")} className="nav__avatar" alt="Avatar del usuario" />
 
-                <ul className="nav__items">
+        <ul className="nav__items">
 
-                    <li className="nav__item">
-                        <a href="home.html" className="nav__item__link"><img src="/src/assets/icon-home.svg"
-                            className="nav__item__icon" alt="Icono del Home" />Home</a></li>
+          <Link to="/home">
+            <li className="nav__item">
+              <a href="home.html" className="nav__item__link"><img src={require("../assets/icon-home.png")}
+                className="nav__item__icon" alt="Icono del Home" />Home</a></li></Link>
 
-                    <li className="nav__item"><a href="calendar.html" className="nav__item__link"><img
-                        src="/src/assets/icon-calendar.svg" className="nav__item__icon" alt="Icono del Calendario" />Calendario</a></li>
+          <Link to="/calendario">
+            <li className="nav__item"><a href="calendar.html" className="nav__item__link"><img
+              src={require("../assets/icon-calendar.png")} className="nav__item__icon" alt="Icono del Calendario" />Calendario</a></li></Link>
 
-                    <li className="nav__item"><a href="settings.html" className="nav__item__link"><img src="/src/assets/icon-settings.svg" className="nav__item__icon" alt="Icono de Configuración" />Configuración</a></li>
+          <Link to="/configuracion">
+            <li className="nav__item"><a href="settings.html" className="nav__item__link"><img src={require("../assets/icon-settings.png")} className="nav__item__icon" alt="Icono de Configuración" />Configuración</a></li></Link>
 
+          <Link to="/">
+            <li className="nav__item"><a href="index.html" className="nav__item__link"><img src={require("../assets/icon-logout.png")}
+              className="nav__item__icon" alt="Icono de Cerrar sesión" />Cerrar sesión</a></li></Link>
+        </ul>
 
-                    <li className="nav__item"><a href="index.html" className="nav__item__link"><img src="/src/assets/icon-logout.svg"
-                        className="nav__item__icon" alt="Icono de Cerrar sesión" />Cerrar sesión</a></li>
-                </ul>
-                
-            </nav>
-        )
-    }
+      </nav>
+    )
+  }
 }
 
 export default Sidebar;
